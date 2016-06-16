2dbe50d480f1d779d9a8f3f49842d0af6b654606
Atul Bansode <abansode@outlook.com>
2016-06-16 12:03:23 +0200

    Update and rename device_P780_ROW.mk to device_P780_CN.mk

--- device_P780_CN.mk
+++ device_P780_CN.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product-if-exists, vendor/lenovo/P780_ROW/P780_ROW-vendor.mk)
+$(call inherit-product-if-exists, vendor/lenovo/P780_CN/P780_CN-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/
 
